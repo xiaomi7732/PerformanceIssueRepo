3fe3d259bd60bc2fd37f42e926834539ce1e9b65
Saar Shen <xiaomi7732@hotmail.com>
2023-02-10 10:04:07 -0800

    Isolate debug endpoint vs prod endpoint

--- src/OPI.WebUI/Program.cs
+++ src/OPI.WebUI/Program.cs
@@ -7,8 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddOPIClient(new Uri("https://opir-test.azurewebsites.net/"));
-builder.Services.AddOPIClient(new Uri("http://localhost:5041/"));
+#if DEBUG
+// builder.Services.AddOPIClient(new Uri("http://localhost:5041/"));
+#else
+builder.Services.AddOPIClient(new Uri("https://opir-test.azurewebsites.net/"));
+#endif
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<CSVReaderFactory>();
 
