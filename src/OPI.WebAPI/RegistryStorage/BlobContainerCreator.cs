23a16efa9efc63f03df95ca26224319b804724b2
Saar Shen <xiaomi7732@hotmail.com>
2023-02-28 14:39:33 -0800

    Dockerize backend API

--- src/OPI.WebAPI/RegistryStorage/BlobContainerCreator.cs
+++ src/OPI.WebAPI/RegistryStorage/BlobContainerCreator.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+
 namespace OPI.WebAPI.RegistryStorage;
 
 internal class BlobContainerCreator : BackgroundService
@@ -16,8 +18,15 @@ internal class BlobContainerCreator : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("Ensure blob container exists");
-        await _registryBlobClient.CreateContainerIfNotExistsAsync(stoppingToken).ConfigureAwait(false);
-        _logger.LogInformation("Ensured blob container exists");
+        try
+        {
+            _logger.LogInformation("Ensure blob container exists");
+            await _registryBlobClient.CreateContainerIfNotExistsAsync(stoppingToken).ConfigureAwait(false);
+            _logger.LogInformation("Ensured blob container exists");
+        }
+        catch (CredentialUnavailableException ex)
+        {
+            _logger.LogError(ex, "Token credential does not exist. Is Managed Identity configured correctly?");
+        }
     }
 }
