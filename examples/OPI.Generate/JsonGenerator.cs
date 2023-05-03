8da3bd675ad84492bd0a200d4b6154083e8b0da1
Jacob Bovee <jabovee@microsoft.com>
2023-05-02 17:18:22 -0700

    Update json generator for formatting and encoding

--- examples/OPI.Generate/JsonGenerator.cs
+++ examples/OPI.Generate/JsonGenerator.cs
@@ -32,7 +32,12 @@ public class JsonGenerator
         }
 
         // Serialize the dictionary
-        string json = JsonSerializer.Serialize(entryDictionary);
+        string json = JsonSerializer.Serialize(entryDictionary, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        });
 
         // Write the json to the output path
         File.WriteAllText(outputPath, json);
