df9980d1d5aaa38806c20eeb4d205a75927c2b0c
Saar Shen 🚾 <saars@microsoft.com>
2023-06-23 11:51:14 -0700

    Allow to display csv

--- src/OPI.WebUI/Pages/IssueBrowser.cs
+++ src/OPI.WebUI/Pages/IssueBrowser.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
@@ -16,7 +17,7 @@ public partial class IssueBrowser
     private IOptions<JsonGenOptions> JsonGenOptions { get; set; } = Options.Create<JsonGenOptions>(new JsonGenOptions());
 
     [Inject]
-    private NavigationManager Navigation {get; set;} = default!;
+    private NavigationManager Navigation { get; set; } = default!;
 
     public bool IsLoading { get; set; }
 
@@ -25,8 +26,8 @@ public partial class IssueBrowser
     private IReadOnlyCollection<PerfIssueItem>? _loadedIssues = null;
     public IEnumerable<PerfIssueItem>? IssueCollection { get; private set; } = null;
 
-
-    public string JsonContent { get; set; } = string.Empty;
+    public string? JsonContent { get; set; } = null;
+    public string? CSVContent { get; set; } = null;
 
     private string? _keyword;
     public string? Keyword
@@ -98,6 +99,7 @@ public partial class IssueBrowser
             _loadedIssues = new List<PerfIssueItem>(await OpiClient.ListAllAsync(_pickedVersion, default)).OrderBy(item => item.LegacyId?.PadLeft(4)).ThenBy(item => item.PermanentId).ToList().AsReadOnly();
             ApplyFilter();
             await UpdateJsonViewAsync(cancellationToken: default);
+            await UpdateCSVViewAsync(cancellationToken: default);
             await UpdateSubstitutesAsync(cancellationToken: default);
             StateHasChanged();
         }
@@ -128,27 +130,105 @@ public partial class IssueBrowser
 
     private async Task UpdateJsonViewAsync(CancellationToken cancellationToken)
     {
-        JsonContent = string.Empty;
-        if (!string.IsNullOrEmpty(_pickedVersion))
+        JsonContent = null;
+        if (string.IsNullOrEmpty(_pickedVersion))
         {
-            List<PerfIssueItem>? allIssueItems = (await OpiClient.ListAllAsync(_pickedVersion, cancellationToken).ConfigureAwait(false)).OrderBy(item => item.LegacyId?.PadLeft(4)).ThenBy(item => item.PermanentId)?.ToList();
-            if (allIssueItems is null || !allIssueItems.Any())
-            {
-                return;
-            }
-
-            PerfIssueRegistryDocument doc = new PerfIssueRegistryDocument()
-            {
-                Schema = JsonGenOptions.Value.SchemaPath,
-                Items = allIssueItems,
-            };
-            JsonSerializerOptions reSerializeOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-            reSerializeOptions.WriteIndented = true;
-            reSerializeOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-
-            JsonContent = JsonSerializer.Serialize<PerfIssueRegistryDocument>(doc, reSerializeOptions);
+            return;
         }
+
+        List<PerfIssueItem>? allIssueItems = (await OpiClient.ListAllAsync(_pickedVersion, cancellationToken).ConfigureAwait(false)).OrderBy(item => item.LegacyId?.PadLeft(4)).ThenBy(item => item.PermanentId)?.ToList();
+        if (allIssueItems is null || !allIssueItems.Any())
+        {
+            JsonContent = string.Empty;
+            return;
+        }
+
+        PerfIssueRegistryDocument doc = new PerfIssueRegistryDocument()
+        {
+            Schema = JsonGenOptions.Value.SchemaPath,
+            Items = allIssueItems,
+        };
+        JsonSerializerOptions reSerializeOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        reSerializeOptions.WriteIndented = true;
+        reSerializeOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+
+        JsonContent = JsonSerializer.Serialize<PerfIssueRegistryDocument>(doc, reSerializeOptions);
     }
+
+    private async Task UpdateCSVViewAsync(CancellationToken cancellationToken)
+    {
+        CSVContent = null;
+        if (string.IsNullOrEmpty(_pickedVersion))
+        {
+            return;
+        }
+
+        List<PerfIssueItem>? allIssueItems = (await OpiClient.ListAllAsync(_pickedVersion, cancellationToken).ConfigureAwait(false)).OrderBy(item => item.LegacyId?.PadLeft(4)).ThenBy(item => item.PermanentId)?.ToList();
+        if (allIssueItems is null || !allIssueItems.Any())
+        {
+            Console.WriteLine("No csv content.");
+            // Nothing
+            CSVContent = string.Empty;
+            return;
+        }
+
+        CSVContent = CreateCSVText(allIssueItems);
+        Console.WriteLine("csv content: {0}", CSVContent);
+    }
+
+    private string CreateCSVText(IEnumerable<PerfIssueItem> items)
+    {
+        StringBuilder builder = new StringBuilder();
+        foreach (PerfIssueItem item in items)
+        {
+            builder.AppendLine(CreateCSVLine(item));
+        }
+        return builder.ToString();
+    }
+
+    private string CreateCSVLine(PerfIssueItem item)
+    {
+        string line = string.Empty;
+        // CSV schema: id,issue*,description*,recommendation*,rationale,docUrl
+
+        // id
+        if (item.PermanentId.HasValue)
+        {
+            line = item.PermanentId.Value.ToString("D");
+        }
+        else if (!string.IsNullOrEmpty(item.LegacyId))
+        {
+            line = $@"""{EscapeCSVField(item.LegacyId)}""";
+        }
+        line += ",";
+
+        // issue
+        line += $@"""{EscapeCSVField(item.Title)}"",";
+
+        // description
+        line += $@"""{EscapeCSVField(item.Description)}"",";
+
+        // Recommendation
+        line += $@"""{EscapeCSVField(item.Recommendation)}"",";
+
+        // rationale
+        if (!string.IsNullOrEmpty(item.Rationale))
+        {
+            line += $@"""{EscapeCSVField(item.Description)}""";
+        }
+        line += ",";
+
+        // docurl
+        if (item.DocURL is not null)
+        {
+            line += $@"""{EscapeCSVField(item.DocURL.AbsoluteUri)}""";
+        }
+
+        return line;
+    }
+
+    private string EscapeCSVField(string content)
+        => content.Replace("\"", "\"\"", StringComparison.Ordinal);
 
     private async Task OnKeywordChanged()
     {
